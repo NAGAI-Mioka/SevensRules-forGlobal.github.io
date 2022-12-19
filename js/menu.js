f74e169b0026dfbc308d3492026332eff067ab90
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-12-20 08:41:13 +0900

    上の階層へボタン実装

--- js/menu.js
+++ js/menu.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $(".button_back").on("click", function () {
         history.back();
     });
+    $(".button_up").on("click", function () {
+        window.location.href = "../index.html";
+    });
 });
