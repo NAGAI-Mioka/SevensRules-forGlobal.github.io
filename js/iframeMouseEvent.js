2fe2893a2e47414303a89b2230708045645fd05a
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-12-20 07:15:58 +0900

    jquery導入、基本のjavascript追加

---
+++ js/iframeMouseEvent.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".iframe_main").iframeTracker({
+        blurCallback: function (event) {
+            //var src = $(this).attr("src");
+            //alert("iframe click!");
+            //console.log(src);
+            console.log($(this._overElement).attr("src"));
+            window.location.href = $(this._overElement).attr("src");
+        },
+        overCallback: function (element, event) {
+            this._overElement = $(element);
+            $(element).removeClass("iframe_out").addClass("iframe_over");
+        },
+        outCallback: function (element, event) {
+            this._overElement = null;
+            $(element).removeClass("iframe_over").addClass("iframe_out");
+        },
+        _overElement: null
+    });
+});
