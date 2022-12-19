54ab305fce820fdeb2beb7263a9275728b54a15b
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-12-20 08:55:56 +0900

    上の階層へボタンの挙動を修正

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
         history.back();
     });
     $(".button_up").on("click", function () {
-        window.location.href = "../index.html";
+        var url = window.location.href;
+        var ary = url.split("/");
+        var str = ary[ary.length - 2] + "/" + ary[ary.length - 1];
+        var rep = url.replace(str, "index.html");
+        window.location.href = rep;
     });
 });
