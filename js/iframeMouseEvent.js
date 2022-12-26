cbb42848ca7854218cc843f3a000785d7f0a7116
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-12-27 05:47:43 +0900

    修正

--- js/iframeMouseEvent.js
+++ js/iframeMouseEvent.js
@@ -1,6 +1,8 @@
 // iframe属性をクリックすると、インライン表示されているページに飛ぶ
 // マウスオーバー、マウスアウト時のCSSを変更する
 
+var iframe_main = document.getElementById("iframe_main");
+
 $(document).ready(function () {
     $(".iframe_child").iframeTracker({
         blurCallback: function (event) {
@@ -17,10 +19,10 @@ $(document).ready(function () {
         _overElement: null
     });
 
-    $("#iframe_main").on("load", function () {
+    $(iframe_main).on("load", function () {
         $(".iframe_child").css("pointer-events", "auto");
     });
-    $("#iframe_main").on("beforeunload", function () {
+    $(iframe_main).on("beforeunload", function () {
         $(".iframe_child").css("pointer-events", "none");
     });
 });
