c6c020121a10d8bf51baf64d8b23861a51b9a998
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-12-26 11:12:59 +0900

    上の階層へボタンが機能するよう修正

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,17 @@
+var iframe_main = document.getElementsById("iframe_main");
+
 $(document).ready(function () {
     $(".button_back").on("click", function () {
         history.back();
     });
     $(".button_up").on("click", function () {
-        var url = window.location.href;
-        var ary = url.split("/");
-        var str = ary[ary.length - 2] + "/" + ary[ary.length - 1];
-        var rep = url.replace(str, "contents.html");
-        window.location.href = rep;
+        var url = element.contentWindow.location.href;
+        if (url !== "https://nagai-mioka.github.io/SevensRules-forGlobal.github.io/contents.html") {
+            var ary = url.split("/");
+            var str = ary[ary.length - 2] + "/" + ary[ary.length - 1];
+            var rep = url.replace(str, "contents.html");
+            window.location.href = rep;
+        }
     });
     $(".button_top").on("click", function () {
         window.location.href = "https://nagai-mioka.github.io/SevensRules-forGlobal.github.io/index.html";
