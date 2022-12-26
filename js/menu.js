1aa273cbbd4565235726cad09c0f2340e379f6b6
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-12-26 15:52:49 +0900

    メニューバー色が変わるよう修正、完了

--- js/menu.js
+++ js/menu.js
@@ -19,7 +19,6 @@ $(document).ready(function () {
 
     $(iframe_main).on("load", function () {
         $(".ul_menuBar a").each(function (i, elem) {
-            console.log($(elem).attr("href"));
             if ($(elem).attr("href") === iframe_main.contentWindow.location.href) {
                 $(elem).removeClass("menu_a_close").addClass("menu_a_open");
             } else {
