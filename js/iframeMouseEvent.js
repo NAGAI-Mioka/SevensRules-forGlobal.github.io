3d56ac65074edf1203817176957c67f0d74299db
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-12-29 23:48:44 +0900

    window.topの確認

--- js/iframeMouseEvent.js
+++ js/iframeMouseEvent.js
@@ -19,8 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     console.log(overIframe);
                     console.log($(overIframe).attr("src"));
                 } else {
-                    window.top.location.href = $(window.top.overIframe).attr("src");
-                    console.log(window.top.overIframe);
+                    console.log(window.top);
                 }
             }
             // if ($(overIframe).attr("src") != null && window.name === "iframe_main") {
