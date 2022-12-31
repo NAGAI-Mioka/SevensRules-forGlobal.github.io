3a32e9a2a1fe4d4ff2c58ce14e73250a0b78236f
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-12-31 22:36:02 +0900

    全ての子ページにcookie適用

--- js/cookieEvent.js
+++ js/cookieEvent.js
@@ -4,7 +4,7 @@ var times_visited = 0;
 // 描画されたとき
 $(window).on("load", function () {
     console.log("load!");
-    if (Cookies.get("visited:" + window.location.pathname) != null) {
+    if (Cookies.get("visited-" + window.location.pathname) != null) {
         $("body").removeClass("body_unread").addClass("body_read");
     } else {
         $("body").removeClass("body_read").addClass("body_unread");
@@ -16,8 +16,8 @@ $(window).on("unload", function () {
     if (window.parent === window.top) {
         console.log("unload!" + window.location.pathname);
         times_visited += 1;
-        if (Cookies.get("visited:" + window.location.pathname) == null) {
-            Cookies.set("visited:" + window.location.pathname, times_visited);
+        if (Cookies.get("visited-" + window.location.pathname) == null) {
+            Cookies.set("visited-" + window.location.pathname, times_visited);
         }
     }
 });
