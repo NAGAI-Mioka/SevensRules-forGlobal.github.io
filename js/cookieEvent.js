764eea03478f9442f4bb2b970de2b18815b5c72f
NAGAI-Mioka <n1910481@edu.cc.uec.ac.jp>
2022-12-31 23:03:25 +0900

    テスト

--- js/cookieEvent.js
+++ js/cookieEvent.js
@@ -1,23 +1,19 @@
 // 一度見たページのデザインを変える
-var times_visited = 0;
 
 // 描画されたとき
 $(window).on("load", function () {
-    console.log("load!");
     if (Cookies.get("visited-" + window.location.pathname) != null) {
-        $("body").removeClass("body_unread").addClass("body_read");
+        $("body, h1").removeClass("unvisited").addClass("visited");
     } else {
-        $("body").removeClass("body_read").addClass("body_unread");
+        $("body, h1").removeClass("visited").addClass("unvisited");
     }
 });
 
 // iframe_mainの表示から外れたとき
 $(window).on("unload", function () {
     if (window.parent === window.top) {
-        console.log("unload!" + window.location.pathname);
-        times_visited += 1;
         if (Cookies.get("visited-" + window.location.pathname) == null) {
-            Cookies.set("visited-" + window.location.pathname, times_visited);
+            Cookies.set("visited-" + window.location.pathname, "true");
         }
     }
 });
